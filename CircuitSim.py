588831799da58ea521acaf19845812b66bd32090
ronripen <Troncho20032>
2022-04-03 12:50:56 +0300

    test

---
+++ CircuitSim.py
@@ -0,0 +1,23 @@
+import sys
+import schemdraw
+import schemdraw.elements as elm
+import ahkab
+from ahkab import circuit, printing, time_functions
+import matplotlib.pylab as plt
+import numpy as np
+
+
+class CircuitSim:
+    def calc(self, data):
+        file = open('test.py', 'a')
+        file.write(data)
+
+
+test = CircuitSim()
+
+
+
+# x = sys.argv[1]
+# z = calculatorObj.calc(x)
+# print(z)
+
