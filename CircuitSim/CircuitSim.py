cbf829e97804fb2a454591813c20397b493a7b7d
ronripen <Troncho20032>
2022-04-05 12:07:57 +0300

    yaml test

--- CircuitSim/CircuitSim.py
+++ CircuitSim/CircuitSim.py
@@ -1,21 +1,21 @@
 import sys
 import schemdraw
 import schemdraw.elements as elm
-#import ahkab
-#from ahkab import circuit, printing, time_functions
+import ahkab
+from ahkab import circuit, printing, time_functions
 import matplotlib.pylab as plt
 import numpy as np
 import yaml
 
 
 def yaml_load(filepath):
-    #Loads a YAML file
+    # Loads a YAML file
     with open(filepath, "r") as Circuit:
         code = yaml.safe_load(Circuit)
     return code
+
+
 yaml.dump(yaml_load('Circuit.yaml'), sys.stdout)
-
-
 
 # class CircuitSim:
 #     def calc(self, data):
@@ -26,8 +26,6 @@ yaml.dump(yaml_load('Circuit.yaml'), sys.stdout)
 # test = CircuitSim()
 
 
-
 # x = sys.argv[1]
 # z = calculatorObj.calc(x)
 # print(z)
-
